9e0c082a60df72b997ec3701cbd00fdc25ea5d61
Sergio Torres <sergio.torres@idr.edu.mx>
2023-08-23 12:45:11 -0600

    ci: funciones increment y decrement

--- src/components/counter/counter.jsx
+++ src/components/counter/counter.jsx
@@ -1,9 +1,28 @@
-
-
 export const Counter = () => {
+  console.log("Estoy dentro del componente Counter");
+
+  let counterValue = 100;
+
+  const handleIncrement = ( event ) => {
+    //console.log(event)
+    console.log("Estoy en fnc increment");
+    counterValue++;
+    console.log("Contador" + counterValue)
+  }
+
+  const handleDecrement = ( e ) => {
+    //console.log(e);
+    console.log("Estoy en la fnc decrement");
+    counterValue--;
+    console.log("Contador" + counterValue)
+  }
+
   return (
     <>
-    <p> {10}</p>
+      <p>Contador: {counterValue}</p>
+      <button onClick={ handleDecrement } > Decrementar</button>
+      <button onClick={ handleIncrement } > Incrementar</button>
+      {/* Agregar botón reset */}
     </>
-  )
-}
+  );
+};
