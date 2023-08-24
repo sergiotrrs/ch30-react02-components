301b2411362bbdf9ea0031e0c5ccda5284c9d2a4
Sergio Torres <sergio.torres@idr.edu.mx>
2023-08-24 14:13:15 -0600

    ci: uso de react router

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,18 @@ import CounterPage from './components/counter/counter-page';
 import Generation from './components/greeting/generation';
 import Greeting from './components/greeting/greeting';
 import { Footer, Navbar } from './components/navbar/navbar';
+import { Routes, Route, Outlet, Link } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <CounterPage />
-      {/* <Greeting />
-      <Generation />
-      <Footer /> */}
+      <Routes>
+         <Route path='/' element={ <Greeting />} />
+         <Route path='/generation' element={<Generation /> } />
+         <Route path='/counter' element={<CounterPage /> } />
+      </Routes>
+      <Footer /> 
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
-
+import { BrowserRouter } from "react-router-dom";
 /*
 React.StrictMode: Componente de React que se utiliza en el desarrollo
 para detectar y advertir sobre posibles problemas en una aplicación.
@@ -19,8 +19,10 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <CssBaseline />
-    <Container  maxWidth="lg">
-      <App />
+    <Container maxWidth="lg">
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Container>
   </React.StrictMode>
 );
